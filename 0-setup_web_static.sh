aa4ab7aee12b70dacec0ac2a30b3781f65a6b7e3
Arowoolo kehinde <arowolokehinde321@gmail.com>
2024-04-04 11:14:37 -0400

    testing

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,8 +2,7 @@
 # script that sets up web servers for the deployment of web_static
 sudo apt-get update
 sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
-
+#commnets
 sudo mkdir -p /data/
 sudo mkdir -p /data/web_static/
 sudo mkdir -p /data/web_static/releases/
